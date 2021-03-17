352ed6cdabc057a0b8256753fd4a3eb7555dd923
Het <hetp111@users.noreply.github.com>
2021-03-17 14:47:57 +0530

    Add MD5, Remove mongoose-encryption

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -17,7 +17,6 @@ const userSchema = new mongoose.Schema({
     password:String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields:["password"] });//1
 
 const User = new mongoose.model("User",userSchema);//and then 2
 
@@ -36,7 +35,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newUser.save(function(err){
         if(err)console.log(err);
@@ -46,7 +45,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
     User.findOne({email:username},function(err,foundUser){
         if(err)console.log(err);
         else{
