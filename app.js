7a225a7e879c4ef27d903118ae8f4556ead9c96e
Het <hetp111@users.noreply.github.com>
2021-03-17 16:09:00 +0530

    Add Level 4 - bcrypt

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -33,26 +33,33 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-    const newUser = new User({
-        email:req.body.username,
-        password:md5(req.body.password)
+
+    bcrypt.hash(req.body.password, 10, function(err, hash) {
+        const newUser = new User({
+            email:req.body.username,
+            password:hash
+        });
+        newUser.save(function(err){
+            if(err)console.log(err);
+            else res.render("secrets");
+        });
     });
-    newUser.save(function(err){
-        if(err)console.log(err);
-        else res.render("secrets");
-    });
+    
 });
 
 app.post("/login",function(req,res){
     const username=req.body.username;
-    const password=md5(req.body.password);
+    const password=req.body.password;
     User.findOne({email:username},function(err,foundUser){
         if(err)console.log(err);
         else{
             if(foundUser){
-                if(foundUser.password===password){
-                    res.render("secrets");
-                }
+
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result===true){
+                        res.render("secrets");
+                    }
+                });
             }
         }
     });
